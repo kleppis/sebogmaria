16ec87025c83ccfde1d9a4cd727ed2de9748293e
Jørgen Kleppan <jokle5828@oslomet.no>
2025-02-13 09:44:12 +0100

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,50 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client"; // Bruk react-dom/client i stedet for react-dom
-import Home from './Home';
-import './index.css';
-import Galleri from './pages/Galleri';
-import Innslag from './pages/Innslag';
-import KommerDu from './pages/KommerDu';
-import Wishlist from './pages/Wishlist';
+import Home from "./Home";
+import "./index.css";
+import Galleri from "./pages/Galleri";
+import Innslag from "./pages/Innslag";
+import KommerDu from "./pages/KommerDu";
+import Wishlist from "./pages/Wishlist";
 import History from "./pages/History";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import InvitasjonPage from "./pages/Invitasjon";
 
 // Opprett routeren
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <Home />,
   },
   {
     path: "innslag", // Endret til små bokstaver for å unngå routingproblemer
-    element: <Innslag />
+    element: <Innslag />,
   },
   {
     path: "galleri", // Endret til små bokstaver for konsistens
-    element: <Galleri />
+    element: <Galleri />,
   },
   {
     path: "kommerdu", // Endret til små bokstaver for konsistens
-    element: <KommerDu />
+    element: <KommerDu />,
   },
   {
     path: "wishlist",
-    element: <Wishlist />
+    element: <Wishlist />,
   },
   {
     path: "history",
-    element: <History />
+    element: <History />,
   },
   {
     path: "invitasjon/:navn",
-    element: <InvitasjonPage />
-  }
+    element: <InvitasjonPage />,
+  },
 ]);
 
 // Få tak i rot-elementet og bruk createRoot fra react-dom/client
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
   throw new Error("Fant ikke root-elementet i HTML-dokumentet.");
@@ -57,5 +54,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>
 );
